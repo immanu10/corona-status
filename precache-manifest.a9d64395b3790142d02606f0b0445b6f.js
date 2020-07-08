094eb7029f265cdcc3ec74ed59b5574718cd257e
immanu10 <54578417+immanu10@users.noreply.github.com>
2020-07-08 17:51:49 +0530

    Updates

---
+++ precache-manifest.a9d64395b3790142d02606f0b0445b6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614970273eb0ac089a47ad3379db047a",
+    "url": "/corona-status/index.html"
+  },
+  {
+    "revision": "b7940fa7889fd948cab6",
+    "url": "/corona-status/static/css/main.3f726515.chunk.css"
+  },
+  {
+    "revision": "63c706e045b08a2d3d5d",
+    "url": "/corona-status/static/js/2.1c417324.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-status/static/js/2.1c417324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7940fa7889fd948cab6",
+    "url": "/corona-status/static/js/main.5205cc0c.chunk.js"
+  },
+  {
+    "revision": "fc0f6db6501611b5d428",
+    "url": "/corona-status/static/js/runtime-main.4353f9d7.js"
+  }
+]);
